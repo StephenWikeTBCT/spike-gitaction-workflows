70e940797605880b76ea093d0f5a0bd71f7e9690
Stephen Wike <60116807+StephenWikeTBCT@users.noreply.github.com>
2020-02-13 16:19:59 -0700

    Update build.ps1

--- ci/build.ps1
+++ ci/build.ps1
@@ -8,6 +8,7 @@ $env:MyLocalVariable = "My temporary test variable."
 
 [System.Environment]::SetEnvironmentVariable('MY_MACHINE_VAR','My Machine scoped test variable',[System.EnvironmentVariableTarget]::Machine)
 
+$MY_EXPLICIT_VAR = 'My Explicit test variable'
 
 Get-ChildItem env:
 
